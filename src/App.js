d87e9de115648a0efb6508cdfae78b4bdadfa3cc
myuser <myemail>
2024-02-08 22:52:19 -0500

    Update App.js
    teste

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import'./style.css';
 function App() {
   return (
     <div className="container">
-      <h1 className="title">Buscador de CEP</h1>
+      <h1 className="title">Buscadores de CEP</h1>
 
       <div className=" containerInput">
         <input type="text" placeholder="Digite seu CEP:" />
