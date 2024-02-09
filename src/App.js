b6ee66e3423ae9a9f0def0ea309d68e0eac1a80d
myuser <myemail>
2024-02-09 06:22:33 -0500

    inserindo api
    instalando e configurando API (viacep)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
-import'./style.css';
+import "./style.css";
+import api from "./services/api"; 
 function App() {
+
+    const [input, setInput] = useState('')
+
+   async function handleSearch(){
+      //01001000/json/ cep escolhido de exemplo
+      if(input === ''){
+        alert("Digite um CEP")
+        return;
+      }
+
+      try{
+          const response = await api.get(`${input}/json/`);
+          console.log(response.data)
+      }catch{
+        alert("Ops, Cep não encontrado!!!")
+        setInput("")
+      }
+    }
+
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
 
       <div className=" containerInput">
-        <input type="text" placeholder="Digite seu CEP:" />
+        <input type="text" placeholder="Digite seu CEP:" 
+        value={input}
+        onChange={(e) => setInput(e.target.value) }/>
 
-        <button className="buttonSearch">
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#fff" />
         </button>
       </div>
 
-      <main className="main"> 
+      <main className="main">
         <h2>CEP: 88807-155</h2>
         <span>Rua teste algum</span>
         <span>Complemento: Alguma coisa</span>
         <span>Vila Azul</span>
         <span>Criciuma SC</span>
       </main>
-
     </div>
   );
 }
